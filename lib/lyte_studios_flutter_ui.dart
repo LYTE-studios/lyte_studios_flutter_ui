35c17f967b8f1ea7774062391b3b4fc4be72ca32
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2024-07-08 14:38:55 +0200

    feature: loading mixins  + svg loaders

--- lib/lyte_studios_flutter_ui.dart
+++ lib/lyte_studios_flutter_ui.dart
@@ -1,4 +1,6 @@
 library lyte_studios_flutter_ui;
 
+export 'mixins/loading_state_mixin.dart';
 export 'theme/lyte_studios_theme.dart';
 export 'translations/lyte_studios_translations.dart';
+export 'ui/icons/svg_icon.dart';
---
+++ lib/ui/icons/svg_icon.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+/// Icon widget using svg images
+class SvgIcon extends StatelessWidget {
+  /// The path for the icon
+  final String icon;
+
+  /// The text color for the icon
+  final Color? color;
+
+  /// Icon size
+  final double size;
+
+  /// Set this to true for colored icons
+  final bool leaveUnaltered;
+
+  const SvgIcon(
+    this.icon, {
+    super.key,
+    this.color,
+    this.size = 16,
+    this.leaveUnaltered = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(
+      icon,
+      height: size,
+      width: size,
+      colorFilter: leaveUnaltered
+          ? const ColorFilter.mode(
+              Colors.transparent,
+              BlendMode.clear,
+            )
+          : ColorFilter.mode(
+              color ?? Theme.of(context).colorScheme.outline,
+              BlendMode.srcIn,
+            ),
+      theme: SvgTheme(
+        fontSize: size,
+      ),
+    );
+  }
+}

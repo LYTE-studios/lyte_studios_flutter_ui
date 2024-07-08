1fd1883171194a43d38496ee9a8f97050a3a49f9
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2024-07-08 15:14:08 +0200

    feature: ink well

--- lib/ui/selectable/clear_ink_well.dart
+++ lib/ui/selectable/clear_ink_well.dart
@@ -3,14 +3,19 @@ import 'package:flutter/material.dart';
 class ClearInkWell extends StatelessWidget {
   final Widget child;
 
+  // This value is not nullable to prevent not clickable areas
+  final Function() onTap;
+
   const ClearInkWell({
     super.key,
     required this.child,
+    required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: onTap,
       overlayColor: WidgetStateColor.transparent,
       highlightColor: Colors.transparent,
       hoverColor: Colors.transparent,

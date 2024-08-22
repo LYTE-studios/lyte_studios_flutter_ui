8a99e6289798c755029605a7f4838f542dda63b5
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2024-08-22 09:09:06 +0200

    Update clear_ink_well.dart

--- lib/ui/selectable/clear_ink_well.dart
+++ lib/ui/selectable/clear_ink_well.dart
@@ -6,16 +6,20 @@ class ClearInkWell extends StatelessWidget {
   // This value is not nullable to prevent not clickable areas
   final Function() onTap;
 
+  final Function()? onLongTap;
+
   const ClearInkWell({
     super.key,
     required this.child,
     required this.onTap,
+    this.onLongTap,
   });
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onTap,
+      onLongPress: onLongTap,
       overlayColor: WidgetStateColor.transparent,
       highlightColor: Colors.transparent,
       hoverColor: Colors.transparent,

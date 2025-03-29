aa1c541bd06aa0ba28f988fa6d71638a04158841
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2025-03-29 14:25:09 +0100

    Update lyte_studios_flutter_ui.dart

--- lib/lyte_studios_flutter_ui.dart
+++ lib/lyte_studios_flutter_ui.dart
@@ -4,3 +4,5 @@ export 'ui/lyte_studios_ui.dart';
 export 'theme/lyte_studios_theme.dart';
 export 'translations/lyte_studios_translations.dart';
 export 'mixins/screen_state_mixin.dart';
+export 'mixins/screen_analytics_mixin.dart';
+export 'services/analytics_service.dart';
